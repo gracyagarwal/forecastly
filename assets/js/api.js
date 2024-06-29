94d1e64c97f58577badc5ad87eacf5dcb6e09c59
Gracy <gracy10.2003@gmail.com>
2024-06-30 02:37:17 +0530

    updated api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,25 +1,38 @@
-const api_key = "3118e474a7e8108b5f9b6b3be1b8791d";
+'use strict';
 
-export const fetchData = function (URL, callback) {
-    fetch(`https://cors.bridged.cc/${URL}&appid=${api_key}`)
-        .then(res => res.json())
-        .then(data => callback(data));
+const api_key ="3118e474a7e8108b5f9b6b3be1b8791d";
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url 
+ * @param {Function} callback callback
+ */ 
+
+export const fetchData = function(URL, callback)
+{
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res => res.json())
+    .then(data=> callback(data));
 }
 
 export const url = {
-    currentWeather(lat, lon) {
+    currentWeather(lat, lon)
+    {
         return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
     },
-    forecast(lat, lon) {
+    forecast(lat, lon){
         return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
     },
-    airPollution(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    airPollution(lat, lon){
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
     },
-    reverseGeo(lat, lon) {
-        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    reverseGeo(lat, lon){
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
     },
-    geo(query) {
-        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    /**
+     * @param {string} query search query eg: "London", "New Delhi"
+     */ 
+    geo(query){
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
     }
 }
