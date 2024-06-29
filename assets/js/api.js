6e809690326a1bfa22c71043d7730ee1584f08ed
Gracy <gracy10.2003@gmail.com>
2024-06-30 02:34:40 +0530

    initial commit

---
+++ assets/js/api.js
@@ -0,0 +1,25 @@
+const api_key = "3118e474a7e8108b5f9b6b3be1b8791d";
+
+export const fetchData = function (URL, callback) {
+    fetch(`https://cors.bridged.cc/${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    geo(query) {
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
